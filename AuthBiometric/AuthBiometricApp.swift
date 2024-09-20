2a965c584397b0bb7110cc1f190b1c2269e947c9
pouyasadri <pouyapaghandeh78@gmail.com>
2024-09-21 01:45:26 +0200

    Initial Commit

---
+++ AuthBiometric/AuthBiometricApp.swift
@@ -0,0 +1,17 @@
+//
+//  AuthBiometricApp.swift
+//  AuthBiometric
+//
+//  Created by Pouya Sadri on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct AuthBiometricApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
