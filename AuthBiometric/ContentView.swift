8e4366cddd204e27f83db2d8476049c5e39ed26c
pouyasadri <pouyapaghandeh78@gmail.com>
2024-09-21 04:43:18 +0200

    initial commit

--- AuthBiometric/ContentView.swift
+++ AuthBiometric/ContentView.swift
@@ -6,17 +6,62 @@
 //
 
 import SwiftUI
+import LocalAuthentication
 
 struct ContentView: View {
+	@State private var isAuthorize = false
+	@State private var showAlert = false
+	@State private var alertMessage : String?{
+		didSet{
+			showAlert = true
+		}
+	}
+	
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+		VStack{
+			if isAuthorize{
+				Text("Welcome, You're authenticated!")
+					.font(.largeTitle)
+			}else{
+				Text("Please authenticate to continue.")
+					.font(.headline)
+					.padding()
+				
+				Button("Authenticate"){
+					authenticate()
+				}
+				.buttonStyle(.borderedProminent)
+				.padding()
+			}
+		}
+		.alert(isPresented: $showAlert){
+			Alert(title: Text("Authentication Error"),message: Text(alertMessage ?? "Error"),dismissButton: .default(Text("OK")))
+		}
     }
+	
+	private func authenticate(){
+		let context = LAContext()
+		var error : NSError?
+		
+		if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error){
+			let reason = "Please authenticate to continue."
+			
+			context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason){
+				success, authenticatedError in
+				
+				DispatchQueue.main.async{
+					if success{
+						self.isAuthorize = true
+					}else{
+						self.isAuthorize = false
+						alertMessage = authenticatedError?.localizedDescription
+					}
+				}
+			}
+		}else{
+			alertMessage = error?.localizedDescription
+		}
+	}
 }
 
 #Preview {
